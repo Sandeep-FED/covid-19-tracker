a4591ccd553107c700b7e0e21a45e554d6e382a5
Sandeep-FED <sandymankara11@gmail.com>
2022-01-24 20:25:43 +0530

    Add: project directory added with components
    Signed-off-by: Sandeep-FED <sandymankara11@gmail.com>

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+// exporting all the components...
+
+export { default as Cards } from "./Cards/cards";
+export { default as Chart } from "./Chart/chart";
+export { default as CountryPicker } from "./Country-picker/country-picker";
