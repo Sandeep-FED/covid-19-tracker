4c3952ebf2ae9f6458904b35a209ad70f4620a0b
Sandeep-FED <sandymankara11@gmail.com>
2022-01-24 20:42:44 +0530

    Added: API directory and made api call in console.
    Signed-off-by: Sandeep-FED <sandymankara11@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
+import { fetchData } from "./API";
 
 export default class app extends Component {
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log(data);
+  }
+
   render() {
     return (
-      <div className={styles.container}> 
+      <div className={styles.container}>
         <Cards />
         <Chart />
         <CountryPicker />
